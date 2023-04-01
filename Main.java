c29c55d31ef6a7acd56bedb3dfa413e03ecee3d0
nkades <nkades04@gmail.com>
2023-04-01 12:41:54 +0600

    1

--- Main.java
+++ Main.java
@@ -6,21 +6,42 @@ public class Main {
         Scanner scanner = new Scanner(System. in);
         int a = scanner.nextInt();
         int n = scanner.nextInt();
-        int[] array = new int[n];
-        for (int i = 0; i < n; i++){
-            array[i] = scanner.nextInt();
-        }
-        switch (a){
-            case 1:
+        switch (a) {
+            case 1 -> {
+                int[] array = cin(n);
                 int min = findMinFromArr(array, n);
                 System.out.println(min);
-            case 2:
-                double avr = findAvrFromArr(array, n, 0);
+            }
+            case 2 -> {
+                int[] arr = cin(n);
+                double avr = findAvrFromArr(arr, n, 0);
                 System.out.println(avr);
+            }
+            case 3 -> System.out.println(PrimeOrNot(n, 2));
         }
 
     }
 
+    private static String PrimeOrNot(int n, int i) {
+        if(n == i){
+            return "Prime";
+        }
+        else if (n % i == 0){
+            return "Composite";
+        }
+        else{
+            return PrimeOrNot(n, i + 1);
+        }
+    }
+
+    private static int[] cin(int n){
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = scanner.nextInt();
+        }
+        return arr;
+    }
     private static double findAvrFromArr(int[] arr, int n, int i) {
         if(i == n - 1){
             return arr[i];
